5155e5ef94992629d762f3b14b8c262055a814de
tegar-erwnoo <mtegarhermawanto@gmail.com>
2024-12-03 21:23:53 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include("koneksi.php");
+
+$sql = 'SELECT * FROM data_barang';
+$result = mysqli_query($conn, $sql);
+
+?>
+
+<?php require('header.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Barang</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Data Barang</h1>
+        <div class="main">
+            <table>
+            <tr>
+                <th>Gambar</th>
+                <th>Nama Barang</th>
+                <th>Kategori</th>
+                <th>Harga Jual</th>
+                <th>Harga Beli</th>
+                <th>Stok</th>
+                <th>Aksi</th>
+            </tr>
+            <?php if($result): ?>
+            <?php while($row = mysqli_fetch_array($result)): ?>
+            <tr>
+            <td><img src="gambar/<?= $row['gambar'];?>" alt="<?= $row['nama'];?>"></td>
+                <td><?= $row['nama'];?></td>
+                <td><?= $row['kategori'];?></td>
+                <td><?= $row['harga_beli'];?></td>      
+                <td><?= $row['harga_jual'];?></td>
+                <td><?= $row['stok'];?></td>
+                <td><?= $row['id_barang'];?></td>
+            </tr>
+                <?php endwhile; else: ?>
+                <tr>
+                    <td colspan="7">Belum ada data</td>
+                </tr>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+    <?php require('footer.php'); ?>
+</body>
+</html>
