22ce0e4c639511d392f37bb9acc4dc70a56e53bf
tegar-erwnoo <mtegarhermawanto@gmail.com>
2024-12-03 21:21:06 +0700

    data_barang.sql

---
+++ data_barang.sql
@@ -0,0 +1,14 @@
+CREATE TABLE data_barang (
+id_barang int(10) auto_increment Primary Key,
+kategori varchar(30),
+nama varchar(30),
+gambar varchar(100),
+harga_beli decimal(10,0),
+harga_jual decimal(10,0),
+stok int(4)
+);
+
+INSERT INTO data_barang (kategori, nama, gambar, harga_beli, harga_jual, stok)
+VALUES ('Elektronik', 'HP Samsung Android', 'hp_samsung.jpg', 2000000, 2400000, 5),
+('Elektronik', 'HP Xiaomi Android', 'hp_xiaomi.jpg', 1000000, 1400000, 5),
+('Elektronik', 'HP OPPO Android', 'hp_oppo.jpg', 1800000, 2300000, 5);
